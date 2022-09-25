ce8b0a4010b52bae6656942fac6dc3f486fe80cd
freddycansic <freddycansick@gmail.com>
2022-09-25 22:29:23 +0100

    glm comparison

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,5 +1,9 @@
 #include "Matrix.h"
 
+#include <chrono>
+
+#include "glm/mat4x4.hpp"
+
 int main()
 {
 	const Mat<4, 4> mat = {
@@ -11,9 +15,31 @@ int main()
 
 	};
 
-	std::cout << mat.determinant() << std::endl;
-	std::cout << mat.inverse() << std::endl;
-	std::cout << mat * mat << std::endl;
+	glm::mat4 glmMat(
+		-5, 4, 1, 7,
+		-9, 3, 2, -5,
+		-2, 0, -1, 1,
+		1, 14, 0, 3
+	);
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	for (size_t i = 0; i < 99999; i++)
+	{
+		mat.inverse();
+	}
+
+	std::cout << (std::chrono::high_resolution_clock::now()-start).count() << std::endl;
+
+	start = std::chrono::high_resolution_clock::now();
+
+	for (size_t i = 0; i < 99999; i++)
+	{
+		glm::inverse(glmMat);
+	}
+
+	std::cout << (std::chrono::high_resolution_clock::now() - start).count() << std::endl;
+
 
 	return 0;
 }
